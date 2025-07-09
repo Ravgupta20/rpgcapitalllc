2d8a96a2830ff06a562c35a7c2c1bf10ccedfc3c
Ravi Gupta <ravgupta20@gmail.com>
2025-07-09 17:36:55 -0400

    setup loading data from json file

--- backend/main.go
+++ backend/main.go
@@ -16,10 +16,29 @@ type FormData struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
+type FormSubmissions struct {
+	Submissions []FormData `json:"submissions"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	http.FileServer(http.Dir("./static"))
 }
 
+func loadSubmissions() FormSubmissions {
+	file, err := os.ReadFile("contacts.json")
+	if err != nil {
+		return FormSubmissions{Submissions: []FormData{}}
+	}
+
+	var submissions FormSubmissions
+	err = json.Unmarshal(file, &submissions)
+	if err != nil {
+		log.Printf("Error unmarshaling JSON: %v", err)
+		return FormSubmissions{Submissions: []FormData{}}
+	}
+
+	return submissions
+}
 func save_contact(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -33,6 +52,9 @@ func save_contact(w http.ResponseWriter, r *http.Request) {
 		Message:   r.PostFormValue("message"),
 		Timestamp: time.Now(),
 	}
+
+	// Load existing submissions
+	submissions := loadSubmissions()
 	// err := json.NewDecoder(r.Body).Decode(&data)
 	// if err != nil {
 	// 	http.Error(w, "Invalid JSON data", http.StatusBadRequest)
