4e5eccf509075633a3a41bde3eece3e4db8e18d2
Ravi Gupta <ravgupta20@gmail.com>
2025-07-09 16:41:39 -0400

    added stati dir to serve static file form go server

--- backend/main.go
+++ backend/main.go
@@ -7,11 +7,19 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Go Server!")
+	http.FileServer(http.Dir("./static"))
 }
+
+func save_contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "save_contact")
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/api/save_contact", save_contact)
 	port := ":8080"
 	fmt.Printf("Server listing on port %s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
+
 }
