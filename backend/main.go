901f58137a095223ad8c5ed3291ba9e3809f5da9
Ravi Gupta <ravgupta20@gmail.com>
2025-07-09 16:15:38 -0400

    added basic go server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Go Server!")
+}
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", handler)
+	port := ":8080"
+	fmt.Printf("Server listing on port %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
